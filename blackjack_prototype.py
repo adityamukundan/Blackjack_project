fd71563e13ac4720e4ce526922f459b2be3eeb42
adityamukundan <aditya.mukundan@gmail.com>
2020-02-18 18:47:08 +0530

    added player turn by turn option for hit and stand

--- blackjack_prototype.py
+++ blackjack_prototype.py
@@ -10,14 +10,52 @@ class cards_deck():
         for i in range(0,2):
             out1.append(random.choice(self.cards))
         return out1
-class player_game(self,chips,bet,cards*):
+
+    def generate_card_single(self):
+        card_single = []
+        for i in range(0, 1):
+            card_single.append(random.choice(self.cards))
+        return card_single
+
 
 def player_input():
     total = 0
     input_1 = cards_deck()
-    player_card = input_1.generate_card()
+    player_card = []
+    player_card.append(input_1.generate_card())
     print(player_card)
     for x in player_card:
-        total = total + input_1.cards_value[x]
+        for y in x:
+            total = total + input_1.cards_value[y]
     print('the cards are {} and total card value is {}'.format(player_card,total))
-player_input()
+    print('edited')
+
+def play_game():
+    input_1 = cards_deck()
+    i=2
+    total = 0
+    new_total = 0
+    player_card = []
+    player_card.append(input_1.generate_card())
+    for x in player_card:
+        for y in x:
+            total = total + input_1.cards_value[y]
+    print('the cards are {} and total card value is {}'.format(player_card, total))
+    bet = input('Please place your bet:\n')
+    bet = int(bet)
+    while i > 1:
+        choice = input('do you want to continue ?\n')
+        if choice == 'hit':
+            input_1 = cards_deck()
+            player_card.append(input_1.generate_card_single())
+            for o in player_card:
+                for z in o:
+                    new_total = new_total +  input_1.cards_value[z]
+            print('the cards are {} and total card value is {}'.format(player_card, new_total))
+            total = new_total
+        elif choice == 'stand':
+            print('the cards are {} and total card value is {}'.format(player_card, total))
+        else:
+            print('thanks for playing')
+            break
+play_game()
