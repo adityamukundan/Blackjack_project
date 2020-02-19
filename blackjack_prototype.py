cbdbb476d72a397aefd94b4e52ac567f1bb715a1
adityamukundan <aditya.mukundan@gmail.com>
2020-02-19 10:28:47 +0530

    added player turn by turn option for hit and stand

--- blackjack_prototype.py
+++ blackjack_prototype.py
@@ -3,6 +3,7 @@ class cards_deck():
     cards = ['Ace',2,3,4,5,6,7,8,9,10,'Jack','Queen','King']
     cards_value = {'Ace':1,2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:10,'Jack':10,'Queen':10,'King':10}
     Deck = {'Diamonds','Clubs','Spades','Heart'}
+
     def Show_deck(self):
         print(self.cards)
     def generate_card(self):
