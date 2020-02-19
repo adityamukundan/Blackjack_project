ab0b5754a4d1b57fb832c439710db9771772cd7b
adityamukundan <aditya.mukundan@gmail.com>
2020-02-19 15:50:40 +0530

    added function to calculate card value

--- blackjack_prototype.py
+++ blackjack_prototype.py
@@ -1,8 +1,9 @@
 import random
 class cards_deck():
     cards = ['Ace',2,3,4,5,6,7,8,9,10,'Jack','Queen','King']
-    cards_value = {'Ace':1,2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:10,'Jack':10,'Queen':10,'King':10}
     Deck = {'Diamonds','Clubs','Spades','Heart'}
+    def __init__(self):
+        pass
 
     def Show_deck(self):
         print(self.cards)
@@ -18,20 +19,22 @@ class cards_deck():
             card_single.append(random.choice(self.cards))
         return card_single
 
-
-
+    def card_value(self,card):
+        new_total = 0
+        cards_value = {'Ace': 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 'Jack': 10, 'Queen': 10,
+                       'King': 10}
+        for o in card:
+            for z in o:
+                new_total = new_total + cards_value[z]
+        print('the cards are {} and total card value is {}'.format(card, new_total))
+        return new_total
 
 def play_game():
     input_1 = cards_deck()
     i=2
-    total = 0
-    new_total = 0
     player_card = []
     player_card.append(input_1.generate_card())
-    for x in player_card:
-        for y in x:
-            total = total + input_1.cards_value[y]
-    print('the cards are {} and total card value is {}'.format(player_card, total))
+    input_1.card_value(player_card)
     bet = input('Please place your bet:\n')
     bet = int(bet)
     while i > 1:
@@ -39,13 +42,9 @@ def play_game():
         if choice == 'hit':
             input_1 = cards_deck()
             player_card.append(input_1.generate_card_single())
-            for o in player_card:
-                for z in o:
-                    new_total = new_total +  input_1.cards_value[z]
-            print('the cards are {} and total card value is {}'.format(player_card, new_total))
-            total = new_total
+            input_1.card_value(player_card)
         elif choice == 'stand':
-            print('the cards are {} and total card value is {}'.format(player_card, total))
+            input_1.card_value(player_card)
         else:
             print('thanks for playing')
             break
