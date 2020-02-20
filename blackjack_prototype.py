6c6c50db19ba01f2382c75f6fc872398e12ebf96
adityamukundan <aditya.mukundan@gmail.com>
2020-02-20 07:51:05 +0530

    added try and except for Value Error in bet

--- blackjack_prototype.py
+++ blackjack_prototype.py
@@ -7,6 +7,7 @@ class cards_deck():
 
     def Show_deck(self):
         print(self.cards)
+
     def generate_card(self):
         out1 = []
         for i in range(0,2):
@@ -21,7 +22,7 @@ class cards_deck():
 
     def card_value(self,card):
         new_total = 0
-        cards_value = {'Ace': 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 'Jack': 10, 'Queen': 10,
+        cards_value = {'Ace': 11, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: 10, 'Jack': 10, 'Queen': 10,
                        'King': 10}
         for o in card:
             for z in o:
@@ -30,24 +31,27 @@ class cards_deck():
         return new_total
 
 def play_game():
-    input_1 = cards_deck()
-    i=2
-    player_card = []
-    player_card.append(input_1.generate_card())
-    input_1.card_value(player_card)
-    bet = input('Please place your bet:\n')
-    bet = int(bet)
-    while i > 1:
-        choice = input('do you want to continue ?\n')
-        if choice == 'hit':
-            input_1 = cards_deck()
-            player_card.append(input_1.generate_card_single())
-            input_1.card_value(player_card)
-        elif choice == 'stand':
-            input_1.card_value(player_card)
-        else:
-            print('thanks for playing')
-            break
+   try:
+       input_1 = cards_deck()
+       i = 2
+       player_card = []
+       player_card.append(input_1.generate_card())
+       input_1.card_value(player_card)
+       bet = int(input('Please place your bet:\n'))
+       while i > 1:
+           choice = input('do you want to continue ?\n')
+           if choice == 'hit':
+               input_1 = cards_deck()
+               player_card.append(input_1.generate_card_single())
+               input_1.card_value(player_card)
+           elif choice == 'stand':
+               input_1.card_value(player_card)
+           else:
+               print('thanks for playing')
+               break
+   except ValueError as msg:
+       print('please enter a number and try again')
+
 play_game()
 
 #class with dealer and game logic to be added with functions .Separate objects to be created for the player and for the dealer at which point the winner is to be decided and the bet money will be calculated with 3:2 ratio
